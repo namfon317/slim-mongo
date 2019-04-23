84fd74e807ba7b39fe5779b2af77d96cee3df590
tumtpk <pk.prateep@gmail.com>
2019-04-23 09:29:20 +0700

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once '/lib/Slim/Slim/Slim.php';
+require_once '/controllers/TestController.php';
+
+  Slim\Slim::registerAutoloader();
+  $app = new Slim\Slim();
+
+  function response($status, $response) {
+    $app = \Slim\Slim::getInstance();
+    $app->status($status);
+    $app->contentType('application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+  }
+
+  $app->get('/', ['TestController', 'index']);
+  $app->post('/insert', function() use($app){
+    TestController::insert($app->request());
+  });
+  $app->get('/search/:name', function ($name) {
+    TestController::search($name);
+  });
+  $app->get('/getdata/:age', function ($age) {
+    TestController::getdata($age);
+  });
+
+  $app->run();
+?>
